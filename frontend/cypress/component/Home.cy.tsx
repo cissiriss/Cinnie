41cd55cfaa31a89e3e8f728561cefd231c474e79
Annie Sahlén <annieimsahlen@gmail.com>
2024-12-05 16:17:16 +0100

    component test on Home-component

---
+++ frontend/cypress/component/Home.cy.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "../../../frontend/src/components/Home";
+
+describe("Home", () => {
+  it("should render", () => {
+    cy.mount(<Home />);
+    cy.get("div").should("have.class", "container");
+    cy.get("div").should("have.class", "prose");
+    cy.get("h1").should("have.text", "Meal planner");
+    cy.get("h2").should("have.text", "Plan your weekly meals");
+    cy.get("ol").should("be.visible");
+    cy.get("li").should("have.length", 3);
+  });
+});
